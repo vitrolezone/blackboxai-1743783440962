244cd233c5fd58afee9c80e29bab97a851e16bd2
habibgurikar <138560156+vitrolezone@users.noreply.github.com>
2025-04-04 21:47:31 +0530

    Add career_counseling/models.py 1743783451093

---
+++ career_counseling/models.py
@@ -0,0 +1,44 @@
+from extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    test_results = db.relationship('TestResult', backref='user', lazy=True)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    options = db.relationship('Option', backref='question', lazy=True)
+
+class Option(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+
+class Career(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    required_skills = db.Column(db.Text)
+    education_path = db.Column(db.Text)
+    salary_range = db.Column(db.String(50))
+    demand = db.Column(db.String(50))
+
+class TestResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_taken = db.Column(db.DateTime, nullable=False)
+    aptitude_score = db.Column(db.Integer)
+    interest_score = db.Column(db.Integer)
+    recommended_careers = db.relationship('CareerRecommendation', backref='test_result', lazy=True)
+
+class CareerRecommendation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    test_result_id = db.Column(db.Integer, db.ForeignKey('test_result.id'), nullable=False)
+    career_id = db.Column(db.Integer, db.ForeignKey('career.id'), nullable=False)
+    match_score = db.Column(db.Float, nullable=False)
+    career = db.relationship('Career', backref='recommendations', lazy=True)
